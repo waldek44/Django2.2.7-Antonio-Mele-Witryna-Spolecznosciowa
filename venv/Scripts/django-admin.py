d87de331dd1e6c666cd2f337e998b35392b189ac
Waldek <reclikmail@gmail.com>
2020-01-28 12:11:23 +0100

    START

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\recli\Desktop\DOJO\antonie_mele_2.2.7_WITRYNA_SPOLECZNOSCIOWA\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
