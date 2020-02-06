1e41c36cf0baaaa15051b3ea03cc39c836656383
Waldek <reclikmail@gmail.com>
2020-02-06 12:13:09 +0100

    4.2 widoki logowania i wylogowania

--- account/views.py
+++ account/views.py
@@ -1,3 +1,47 @@
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
 from django.shortcuts import render
+from .forms import LoginForm
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+'''
+1) pobiera formularz z forms.py i przypisuje go do zmiennej form,
+    która tworzy nowy egzemplarz formularza logowania wraz z wysłanymi danymi i wyświetli go w szablonie account/login
+    
+2) is_valid() - waliduje czy wszystkie pola formularza są poprawne
+
+3) metoda authenticate() - jeżeli pola formularza są poprawne to za pomocą metody authenticate uwierzytelniam 
+   użytkownika na podstawie informacji przechowywanych w db metoda authenticate pobiera username i password a zwraca 
+   obiekt user jeśli użytkownik zostanie uwierzytelniony lub None wraz z odpowiednim komunikatem
+   
+4) is_active - atrybut modelu User - sprawdza czy konto użytkownika jest aktywne
+
+5) metoda login() - jeśli konto aktywne to loguje i rozpoczyna sesję (umieszcza użytkownika w sesji)
+'''
+
+
+# widok logowania użytkownika
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(username=cd['username'],
+                                password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Uwierzytelnienie zakończyło się sukcesem.')
+                else:
+                    return HttpResponse('Konto jest zablokowane.')
+            else:
+                return HttpResponse('Nieprawidłowe dane uwierzytelniające.')
+    else:
+        form = LoginForm()
+    return render(request, 'account/login.html', {'form': form})
+
+
+@login_required
+def dashboard(request):
+    return render(request, 'account/dashboard.html', {'section': 'dashboard'})
--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'account.apps.AccountConfig',  # ten mój account
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+    ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pl'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Warsaw'
 
 USE_I18N = True
 
@@ -116,5 +117,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
 STATIC_URL = '/static/'
+STATICFILES_DIRS = ['static']
+
+# zmienne wymagane do poprawnej pracy login_required
+LOGIN_REDIRECT_URL = 'dashboard'  # URL do którego przekieruje jeśli widok login nie otrzyma parametru next
+LOGIN_URL = 'login'  # URL na który przekieruje po zalogowaniu użytkownika (np. za pomocą login_required)
+LOGOUT_URL = 'logout'  # URL na który przekieruje po wylogowaniu użytkownika (np. za pomocą login_required)
